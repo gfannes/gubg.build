c00a66754255ba1081ef78b7c1273d6ce67ed39e
Geert Fannes <geert.fannes@gmail.com>
2015-09-11 23:00:10 +0200

    added basic rake-based build system for C++, but with support for recursive header dependency detection

---
+++ rakefile.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH << Dir.pwd
+require('gubg/build/Executable.rb')
+
+exe = Build::Executable.new('test.exe')
+exe.set_object_dir('.objects')
+exe.add_sources(FileList.new('test/*'))
+
+task :clean do
+    exe.clean
+end
+task :build do
+    exe.build
+end
+task :test => :build do
+    exe.run
+end
