5d8ccd11c188c9bf44cc5e4f5bdd330286dfafb8
Geert Fannes <geert.fannes@gmail.com>
2015-09-11 23:16:07 +0200

    some rework to have a need for adding the include path

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,8 @@ require('gubg/build/Executable.rb')
 
 exe = Build::Executable.new('test.exe')
 exe.set_object_dir('.objects')
-exe.add_sources(FileList.new('test/*'))
+exe.add_sources(FileList.new('test/**/*.*'))
+exe.add_include_path('test/inc')
 
 task :clean do
     exe.clean
