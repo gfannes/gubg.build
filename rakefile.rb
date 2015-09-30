edf88e627abaa5f5ca947fc468de87ece0936399
Geert Fannes <geert.fannes@gmail.com>
2015-09-30 21:58:56 +0200

    moved shared.rb from gubg.tools and reworked the rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,17 +1,52 @@
-$LOAD_PATH << Dir.pwd
-require('gubg/build/Executable.rb')
+shared_fn = File.join(ENV['gubg'], 'shared.rb')
+if File.exist?(shared_fn)
+    require(shared_fn)
+else
+    puts("Bootstrapping \"shared.rb\"")
+    require('./shared.rb')
+    GUBG::publish('./', 'shared.rb')
+end
+include GUBG
 
-exe = Build::Executable.new('test.exe')
-exe.set_object_dir('.objects')
-exe.add_sources(FileList.new('test/**/*.*'))
-exe.add_include_path('test/inc')
+task :default => :help
+task :help do
+    puts('declare: copy all scripts and headers to GUBG::shared')
+    puts('define: build and copy libraries and executables to GUBG::shared')
+end
 
 task :clean do
-    exe.clean
+    Rake::Task['test:clean'].invoke
 end
-task :build do
-    exe.build
+
+task :declare do
+    publish('./', 'shared.rb')
+    publish('src', '**/*.rb', dst: 'ruby')
 end
-task :test => :build do
-    exe.run
+
+task :define => :declare do
+end
+
+task :test do
+    Rake::Task['test:build'].invoke
+    Rake::Task['test:test'].invoke
+end
+
+namespace :test do
+    exe = nil
+    task :setup do
+        require('gubg/build/Executable.rb')
+        exe = Build::Executable.new('test.exe')
+        exe.set_object_dir('.objects')
+        exe.add_sources(FileList.new('test/**/*.*'))
+        exe.add_include_path('test/inc')
+    end
+    task :build => :setup do
+        exe.build
+    end
+    task :clean => :setup do
+        exe.clean
+    end
+    task :test => :build do
+        exe.run
+    end
 end
---
+++ shared.rb
@@ -0,0 +1,66 @@
+require('fileutils')
+
+module GUBG
+    def self.shared(*parts)
+        raise('ERROR: You have to specify the shared destination dir via the environment vairable "gubg"') unless ENV.has_key?('gubg')
+        File.join(ENV['gubg'], *parts.compact)
+    end
+    def shared(*parts)
+        GUBG::shared(*parts)
+    end
+
+    def self.shared_file(*parts)
+        fn = shared(*parts)
+        raise("File \"#{fn}\" does not exist") unless File.exist?(fn)
+        fn
+    end
+    def shared_file(*parts)
+        GUBG::shared_file(*parts)
+    end
+
+    def self.shared_dir(*parts)
+        dir = shared(*parts)
+        FileUtils.mkdir_p(dir) unless File.exist?(dir)
+        dir
+    end
+    def shared_dir(*parts)
+        GUBG::shared_dir(*parts)
+    end
+
+    def self.publish(src, pattern, na = {dst: nil, mode: nil})
+        dst = shared(na[:dst])
+        Dir.chdir(src) do
+            FileList.new(pattern).each do |fn|
+                dst_fn = File.join(dst, fn)
+                dst_dir = File.dirname(dst_fn)
+                FileUtils.mkdir_p(dst_dir) unless File.exist?(dst_dir)
+                if (!File.exist?(dst_fn) or !FileUtils.identical?(fn, dst_fn))
+                    puts("Installing \"#{fn}\" to \"#{dst_fn}\"")
+                    FileUtils.install(fn, dst_dir, mode: na[:mode])
+                end
+            end
+        end
+    end
+    def publish(src, pattern, na = {})
+        GUBG::publish(src, pattern, na)
+    end
+
+    def self.link_unless_exists(old, new)
+        ln_s(old, new) unless (File.exist?(new) or File.symlink?(new))
+    end
+    def link_unless_exists(old, new)
+        GUBG::link_unless_exists(old, new)
+    end
+
+    def self.git_clone(uri, name)
+        if not File.exist?(name)
+            Rake.sh("git clone #{uri}/#{name}")
+            Dir.chdir(name) {yield} if block_given?
+        end
+    end
+    def git_clone(uri, name)
+        GUBG::git_clone(uri, name)
+    end
+end
+
+$LOAD_PATH << GUBG::shared('ruby')
