bbbfe2dbd5f83bfb711ad5400bf311919a7660ed
Geert Fannes <geert.fannes@gmail.com>
2016-01-26 21:29:42 +0100

    added force include support

--- src/gubg/build/GCC.rb
+++ src/gubg/build/GCC.rb
@@ -8,11 +8,13 @@ module Build
         def compile_command(object, source, type)
             include_paths_cmd = @include_paths.map{|ip|"-I#{ip}"}*' '
             defines_cmd = @defines.map{|ip|"-D#{ip}"}*' '
+            force_includes_cmd = @force_includes.map{|fi|"-include #{fi}"}*' '
+            options_cmd = @options.map{|o|"-#{o}"}*' '
             case type
             when :cpp
-                "g++ -std=c++11 -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd}"
+                "g++ -std=c++11 -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd} #{force_includes_cmd} #{options_cmd}"
             when :c
-                "gcc -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd}"
+                "gcc -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd} #{force_includes_cmd} #{options_cmd}"
             else raise("Unknown source type #{type}") end
         end
         def link_command(type, fn, objects)
