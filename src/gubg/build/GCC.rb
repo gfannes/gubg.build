e6ed15f746a1715c2d8d299f3069c259972f5edf
Geert Fannes <geert.fannes@gmail.com>
2016-05-03 20:23:52 +0200

    integrated gcc version check

--- src/gubg/build/GCC.rb
+++ src/gubg/build/GCC.rb
@@ -2,20 +2,35 @@ require('gubg/build/Compiler.rb')
 
 module Build
     class GCC < Compiler
+        def GCC.version()
+            return @version_ if @version_
+            re = /(\d+)\.(\d+)\.(\d+)$/
+            output = `gcc --version`.split('\n')[0]
+            if md = re.match(output)
+                @version_ = md[1].to_i*10+md[2].to_i
+            else
+                raise("Could not parse version from #{output}")
+            end
+            return @version_
+        end
         def libname(name)
             "lib#{name}.a"
+        end
+        def color_cmd()
+            (GCC.version >= 49 ? '-fdiagnostics-color' : '')
         end
         def compile_command(object, source, type)
             include_paths_cmd = @include_paths.map{|ip|"-I#{ip}"}*' '
             defines_cmd = @defines.map{|ip|"-D#{ip}"}*' '
             force_includes_cmd = @force_includes.map{|fi|"-include #{fi}"}*' '
             options_cmd = @options.map{|o|"-#{o}"}*' '
-            cpp_standard_cmd = "-std=#{@cpp_standard || 'c++14'}"
+            default_std = (GCC.version >= 49 ? 'c++14' : 'c++11')
+            cpp_standard_cmd = "-std=#{@cpp_standard || default_std}"
             case type
             when :cpp
-                "g++ -fdiagnostics-color #{cpp_standard_cmd} -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd} #{force_includes_cmd} #{options_cmd}"
+                "g++ #{color_cmd} #{cpp_standard_cmd} -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd} #{force_includes_cmd} #{options_cmd}"
             when :c
-                "gcc -fdiagnostics-color -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd} #{force_includes_cmd} #{options_cmd}"
+                "gcc #{color_cmd} -c #{source} -o #{object} #{include_paths_cmd} #{defines_cmd} #{force_includes_cmd} #{options_cmd}"
             else raise("Unknown source type #{type}") end
         end
         def link_command(type, fn, objects)
@@ -25,7 +40,7 @@ module Build
                 else nil end
             end.compact*' '
             case type
-            when :exe then "g++ -fdiagnostics-color #{options_cmd} -o #{fn} #{objects*' '} #{lib_sp_cli} #{lib_cli}"
+            when :exe then "g++ #{color_cmd} #{options_cmd} -o #{fn} #{objects*' '} #{lib_sp_cli} #{lib_cli}"
             when :lib then "ar rcs #{fn} #{objects*' '}"
             else raise("Unknown link type #{type}") end
         end
