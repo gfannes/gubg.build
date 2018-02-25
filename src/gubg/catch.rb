bf79a12fb41b484527892208792795b9852b4a70
Geert Fannes <geert.fannes@gmail.com>
2018-02-25 21:46:53 +0100

    [dev](Basic colors)

--- src/gubg/catch.rb
+++ src/gubg/catch.rb
@@ -1,13 +1,52 @@
 module GUBG
     module Catch
+        class Color
+            def initialize()
+                @stack = [:white]
+            end
+            def push(sym)
+                @stack.push(sym)
+                color_(@stack.last)
+            end
+            def pop()
+                @stack.pop
+                color_(@stack.last)
+            end
+            def scope(sym, &block)
+                print push(sym)
+                block.call
+                print pop
+            end
+            private
+            def color_(sym = nil)
+                sym ||= :grey
+                case sym
+                when :white then "\033[1;37m"
+                when :grey then "\033[0;37m"
+                when :green then "\033[0;32m"
+                when :red then "\033[0;31m"
+                when :blue then "\033[0;34m"
+                when :purple then "\033[0;35m"
+                when :cyan then "\033[0;36m"
+
+                when :yellow then "\033[1;33m"
+                when :lightblue then "\033[1;34m"
+                when :lightpurple then "\033[1;35m"
+                when :lightcyan then "\033[1;36m"
+                end
+            end
+        end
+
         class TestCase
             attr(:name)
             def initialize(name, &block)
                 @name, @block = name, block
+                @level = 0
+                @color = Color.new
             end
 
             def section(name, &block)
-                indent = "  "*@head
+                print @color.push(:white)
                 @head += 1
                 begin
                     @path << -1 if @path.size <= @head
@@ -21,9 +60,13 @@ module GUBG
                                 #We already saw a leaf node and will hence skip all the rest. We do need to check
                                 #hereunder if we have a sibling.
                             else
-                                puts(indent+"  [section](name:#{name}){")
+                                @color.scope(:lightblue){puts(indent_+"[section](name:#{name}){")}
+                                @level += 1
+                                print @color.push(:grey)
                                 block.call
-                                puts(indent+"  }")
+                                print @color.pop
+                                @level -= 1
+                                @color.scope(:lightblue){puts(indent_+"}")}
                                 @saw_leaf = true if @state.size > @head
                             end
                         elsif ix > state[:follow_ix]
@@ -33,6 +76,7 @@ module GUBG
                     @path.pop if @path.size > @head+1
                 end
                 @head -= 1
+                print @color.pop
             end
 
             def must(expr)
@@ -45,19 +89,28 @@ module GUBG
             end
 
             def run(summary)
-                puts "[test_case](name:#{@name}){"
+                print @color.push(:white)
+                @color.scope(:lightpurple){puts "[test_case](name:#{@name}){"}
+                @level += 1
+                
                 @summary = summary
                 #Indicates the path that should be followed to the first leaf. has_sibling is extra info
                 #that is used later to update the @state
                 @state = [{follow_ix: 0, has_sibling: false}]
                 loop do
+                    @color.scope(:lightcyan){puts(indent_+"[iteration]{")}
+
                     #Info that reflects where we currently are in the tree
                     @path = [0]
                     @head = 0
 
                     @saw_leaf = false
 
+                    @level += 1
+                    print @color.push(:grey)
                     @block.call
+                    print @color.pop
+                    @level -= 1
 
                     while !@state.empty?
                         state = @state.last
@@ -72,19 +125,28 @@ module GUBG
                         end
                     end
 
+                    @color.scope(:lightcyan){puts(indent_+"}")}
+
                     break if @state.empty?
                 end
-                puts "}"
+                @level -= 1
+                @color.scope(:lightpurple){puts "}"}
+                print @color.pop
             end
             private
+            def indent_()
+                "  "*@level
+            end
             def pf_(expr)
+                print @color.push(:white)
                 pf = (expr ? :passed : :failed)
                 case pf
                 when :passed
-                    puts("[passed]")
+                    @color.scope(:green){puts(indent_+"[passed]")}
                 when :failed
-                    puts("[FAILED]")
+                    @color.scope(:red){puts(indent_+"[FAILED]")}
                 end
+                @color.pop
                 pf
             end
         end
@@ -135,7 +197,17 @@ module GUBG
                     tc.run(summary)
                 end
             end
-            puts summary
+            color = Color.new
+            print color.push(:white)
+            print("[summary]")
+            [:available_test_cases, :selected_test_cases].each{|sym|print("(#{sym}:#{summary[sym]})")}
+            puts("{")
+            [:passed, :failed].each do |pf|
+                c = {passed: :green, failed: :red}[pf]
+                color.scope(c){puts("  [#{pf}](count:#{summary[pf]})") } if summary[pf] > 0
+            end
+            puts("}")
+            print color.pop
         end
     end
 end
