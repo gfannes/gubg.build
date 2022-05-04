64489b7736d77033461cc1b1ca5fd784b683d3c6
Geert Fannes <geert.fannes@gmail.com>
2022-05-04 20:38:03 +0200

    fix

--- rake.rb
+++ rake.rb
@@ -14,12 +14,12 @@
 		end
 
 		$gubg__dir.each do |name, dir|
-			full_task_name = "#{name}:#{task_name}"
+			full_task_name = "gubg:#{name}:#{task_name}"
 			if Rake::Task.task_defined?(full_task_name)
 				task = Rake::Task[full_task_name]
-				puts(">> #{task_name} for #{name}")
+				puts(">> #{task_name} for gubg.#{name}")
 				task.execute()
-				puts("<< #{task_name} for #{name}")
+				puts("<< #{task_name} for gubg.#{name}")
 				puts()
 			end
 		end
