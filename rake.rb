0f542c370ef8847cc9a7b66c9912c4a661e561a2
Geert Fannes <geert.fannes@gmail.com>
2022-05-04 14:11:43 +0200

    Added cleaning of files in root

--- rake.rb
+++ rake.rb
@@ -9,6 +9,14 @@ end
 }.each do |task_name, description|
 	desc description
 	task(task_name) do
+		case task_name
+		when :clean
+			%w[resp gnuplot ninja a wav log svg].each do |ext|
+				files = FileList.new("*.#{ext}")
+				rm(files) unless files.empty?()
+			end
+		end
+
 		$gubg__dir.each do |name, dir|
 			full_task_name = "#{name}:#{task_name}"
 			if Rake::Task.task_defined?(full_task_name)
